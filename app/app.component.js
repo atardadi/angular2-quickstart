38baffe3690f829528067049d8406722d6a5109f
Dadi Atar <atardadi@gmail.com>
2015-12-27 09:18:15 +0200

    unstable - in the middle of routing

--- app/app.component.js
+++ app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', './hero-async-message.component'], function(exports_1) {
+System.register(['angular2/core', 'angular2/router', './crisis-list.component', './hero-list.component'], function(exports_1) {
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -8,15 +8,21 @@ System.register(['angular2/core', './hero-async-message.component'], function(ex
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, hero_async_message_component_1;
+    var core_1, router_1, crisis_list_component_1, hero_list_component_1;
     var AppComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
             },
-            function (hero_async_message_component_1_1) {
-                hero_async_message_component_1 = hero_async_message_component_1_1;
+            function (router_1_1) {
+                router_1 = router_1_1;
+            },
+            function (crisis_list_component_1_1) {
+                crisis_list_component_1 = crisis_list_component_1_1;
+            },
+            function (hero_list_component_1_1) {
+                hero_list_component_1 = hero_list_component_1_1;
             }],
         execute: function() {
             AppComponent = (function () {
@@ -25,9 +31,21 @@ System.register(['angular2/core', './hero-async-message.component'], function(ex
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: "<hero-message></hero-message>",
-                        directives: [hero_async_message_component_1.HeroAsyncMessageComponent]
-                    }), 
+                        template: "\n\t\t<h1>Component Router</h1>\n\t\t<a [routerLink]=\"['CrisisCenter']\">Crisis Center</a>\n    <a [routerLink]=\"['Heroes']\">Heroes</a>\n\t\t<router-outlet></router-outlet>\n  ",
+                        directives: [router_1.ROUTER_DIRECTIVES]
+                    }),
+                    router_1.RouteConfig([
+                        {
+                            path: '/crisis-center',
+                            name: 'CrisisCenter',
+                            component: crisis_list_component_1.CrisisListComponent
+                        },
+                        {
+                            path: '/heroes',
+                            name: 'Heroes',
+                            component: hero_list_component_1.HeroListComponent
+                        }
+                    ]), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
                 return AppComponent;
